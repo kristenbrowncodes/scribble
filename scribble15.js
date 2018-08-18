9ee502f68e73518cd4494f3ddcafaa9432875d41
kristenbrowncodes <42063362+kristenbrowncodes@users.noreply.github.com>
2018-08-18 06:57:50 -0700

    Update scribble15.js

--- scribble15.js
+++ scribble15.js
@@ -172,8 +172,8 @@ function matchMed(mM){
         r=0; g=0; b=0; a=255;
 
         // Select a fill style
-        ctx.fillStyle = 'purple';
-	ctx.globalAlpha = alpha;
+        ctx.fillStyle = "rgba("+r+","+g+","+b+","+(a/255)+")";
+	//ctx.globalAlpha = alpha;
 	 //ctx. lineWidth = thickness;
 
         // Draw a filled circle
